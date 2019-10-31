23b2026b778dad57b7035ba03a343828e0c073d5
rymon23 <rymon23@gmail.com>
2019-10-31 12:49:47 -0400

    Fix reviews foreign key reference

--- db/migrate/20191014153239_update_reviews.rb
+++
@@ -1,5 +0,0 @@
-class UpdateReviews < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :reviews, :author_id, :user_id
-  end
-end
--- db/migrate/20191014153801_update_to_reviews.rb
+++
@@ -1,4 +0,0 @@
-class UpdateToReviews < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
